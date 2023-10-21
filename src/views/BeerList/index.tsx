03ec7d72446a800170dc1e5e538642471e542f9d
omr-korkmaz <h.omr.korkmaz@gmail.com>
2023-10-21 20:19:02 +0200

    fix and move the filter sort pagination to Beer List page (fix the misunderstanding)

--- src/views/BeerList/index.tsx
+++ src/views/BeerList/index.tsx
@@ -1,16 +1,64 @@
 import { useEffect, useState } from 'react';
 import { Beer } from '../../types';
 import { fetchData } from './utils';
-import { Avatar, List, ListItemAvatar, ListItemButton, ListItemText } from '@mui/material';
+import { Avatar, Button, List, ListItemAvatar, ListItemButton, ListItemText, TextField } from '@mui/material';
 import SportsBar from '@mui/icons-material/SportsBar';
 import { useNavigate } from 'react-router-dom';
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import styles from "./BeerList.module.css";
+
+
+const ITEMS_PER_PAGE = 5;
+
 
 const BeerList = () => {
   const navigate = useNavigate();
   const [beerList, setBeerList] = useState<Array<Beer>>([]);
+  const [filterText, setFilterText] = useState<string>("");
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [sortBy, setSortBy] = useState<string | null>(null);
 
   // eslint-disable-next-line
   useEffect(fetchData.bind(this, setBeerList), []);
+
+  
+  const totalPages = Math.ceil(beerList.length / ITEMS_PER_PAGE);
+
+  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterText(event.target.value);
+    setCurrentPage(1);
+  };
+
+  const handleSort = (criteria: string) => {
+    setSortBy(criteria);
+
+    setCurrentPage(1);
+  };
+
+  const sortBeerList = (list: Beer[]) => {
+    if (sortBy === 'name') {
+      return list.slice().sort((a, b) => a.name.localeCompare(b.name));
+    }
+    return list;
+  };
+
+
+  
+  const filteredBeerList = beerList.filter((beer) =>
+    beer.name.toLowerCase().includes(filterText.toLowerCase())
+  );
+
+  const sortedBeerList = sortBeerList(filteredBeerList);
+
+
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+
+  // const currentBeerList = filteredBeerList.slice(startIndex, endIndex);
+
+  const currentBeerList = sortedBeerList.slice(startIndex, endIndex);
+
 
   const onBeerClick = (id: string) => navigate(`/beer/${id}`);
 
@@ -21,8 +69,27 @@ const BeerList = () => {
           <h1>BeerList page</h1>
         </header>
         <main>
+
+        <div>
+                <TextField
+                  label="Filter..."
+                  variant="outlined"
+                  value={filterText}
+                  onChange={handleFilterChange}
+                />
+
+{/* <DataGrid rows={rows} columns={columns} /> */}
+
+<Button variant="contained" onClick={() => handleSort('name')}>
+                  Sort by Name
+                </Button>
+              
+              </div>
           <List>
-            {beerList.map((beer) => (
+
+
+
+            {currentBeerList.map((beer) => (
               <ListItemButton key={beer.id} onClick={onBeerClick.bind(this, beer.id)}>
                 <ListItemAvatar>
                   <Avatar>
@@ -33,6 +100,31 @@ const BeerList = () => {
               </ListItemButton>
             ))}
           </List>
+          <div className={styles.pagination}>
+                <Button
+                  disabled={currentPage === 1}
+                  onClick={() => setCurrentPage(currentPage - 1)}
+                  startIcon={<ArrowBackIcon />}
+                  variant="contained"
+                >
+                  Previous Page
+                </Button>
+                <Button
+                  disabled={endIndex >= filteredBeerList.length}
+                  onClick={() => setCurrentPage(currentPage + 1)}
+                  endIcon={<ArrowForwardIcon />}
+                  variant="contained"
+                >
+                  Next Page
+                </Button>
+
+                <span>
+                  {" "}
+                  {/* ( Page {currentPage} of {totalPages} ){" "} */}
+                </span>
+              </div>
+
+          
         </main>
       </section>
     </article>
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,65 +1,16 @@
-import { useEffect, useState } from "react";
-import { fetchData } from "./utils";
-import { Beer } from "../../types";
-import { Link as RouterLink } from "react-router-dom";
-import { Button, Checkbox, Paper, TextField, Link } from "@mui/material";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-// import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
-// import { useDemoData } from '@mui/x-data-grid-generator';
-
-import styles from "./Home.module.css";
-
-const ITEMS_PER_PAGE = 5;
+import { useEffect, useState } from 'react';
+import { fetchData } from './utils';
+import { Beer } from '../../types';
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Checkbox, Paper, TextField, Link } from '@mui/material';
+import styles from './Home.module.css';
 
 const Home = () => {
   const [beerList, setBeerList] = useState<Array<Beer>>([]);
   const [savedList, setSavedList] = useState<Array<Beer>>([]);
-  const [filterText, setFilterText] = useState<string>("");
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const [sortBy, setSortBy] = useState<string | null>(null);
-
-
-  const totalPages = Math.ceil(beerList.length / ITEMS_PER_PAGE);
 
   // eslint-disable-next-line
   useEffect(fetchData.bind(this, setBeerList), []);
-
-  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFilterText(event.target.value);
-    setCurrentPage(1);
-  };
-
-  const handleSort = (criteria: string) => {
-    setSortBy(criteria);
-
-    setCurrentPage(1);
-  };
-
-  const sortBeerList = (list: Beer[]) => {
-    if (sortBy === 'name') {
-      return list.slice().sort((a, b) => a.name.localeCompare(b.name));
-    }
-    return list;
-  };
-
-
-
-
-  const filteredBeerList = beerList.filter((beer) =>
-    beer.name.toLowerCase().includes(filterText.toLowerCase())
-  );
-
-  const sortedBeerList = sortBeerList(filteredBeerList);
-
-
-  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-  const endIndex = startIndex + ITEMS_PER_PAGE;
-
-  // const currentBeerList = filteredBeerList.slice(startIndex, endIndex);
-
-  const currentBeerList = sortedBeerList.slice(startIndex, endIndex);
-
 
   return (
     <article>
@@ -68,24 +19,11 @@ const Home = () => {
           <Paper>
             <div className={styles.listContainer}>
               <div className={styles.listHeader}>
-                <TextField
-                  label="Filter..."
-                  variant="outlined"
-                  value={filterText}
-                  onChange={handleFilterChange}
-                />
-
-{/* <DataGrid rows={rows} columns={columns} /> */}
-
-<Button variant="contained" onClick={() => handleSort('name')}>
-                  Sort by Name
-                </Button>
-              
-                <Button variant="contained">Reload list</Button>
+                <TextField label='Filter...' variant='outlined' />
+                <Button variant='contained'>Reload list</Button>
               </div>
-
               <ul className={styles.list}>
-                {currentBeerList.map((beer, index) => (
+                {beerList.map((beer, index) => (
                   <li key={index.toString()}>
                     <Checkbox />
                     <Link component={RouterLink} to={`/beer/${beer.id}`}>
@@ -94,30 +32,6 @@ const Home = () => {
                   </li>
                 ))}
               </ul>
-
-              <div className={styles.pagination}>
-                <Button
-                  disabled={currentPage === 1}
-                  onClick={() => setCurrentPage(currentPage - 1)}
-                  startIcon={<ArrowBackIcon />}
-                  variant="contained"
-                >
-                  Previous Page
-                </Button>
-                <Button
-                  disabled={endIndex >= filteredBeerList.length}
-                  onClick={() => setCurrentPage(currentPage + 1)}
-                  endIcon={<ArrowForwardIcon />}
-                  variant="contained"
-                >
-                  Next Page
-                </Button>
-
-                <span>
-                  {" "}
-                  ( Page {currentPage} of {totalPages} ){" "}
-                </span>
-              </div>
             </div>
           </Paper>
 
@@ -125,7 +39,7 @@ const Home = () => {
             <div className={styles.listContainer}>
               <div className={styles.listHeader}>
                 <h3>Saved items</h3>
-                <Button variant="contained" size="small">
+                <Button variant='contained' size='small'>
                   Remove all items
                 </Button>
               </div>
