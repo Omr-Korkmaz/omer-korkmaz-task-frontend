175560d6bc40d6bbe929e51856a53e34347f5b56
omr-korkmaz <h.omr.korkmaz@gmail.com>
2023-10-28 20:05:58 +0200

    add navigation and style to grid data to get beer page via clicking

--- src/views/BeerList/index.tsx
+++ src/views/BeerList/index.tsx
@@ -1,120 +1,113 @@
-import { useEffect, useState } from 'react';
-import { Beer } from '../../types';
-import { fetchData } from './utils';
-import { Avatar, Button, List, ListItemAvatar, ListItemButton, ListItemText, TextField } from '@mui/material';
-import SportsBar from '@mui/icons-material/SportsBar';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Beer } from "../../types";
+import { fetchData } from "./utils";
+import {
+  Avatar,
+  Button,
+  List,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText,
+  TextField,
+} from "@mui/material";
+import SportsBar from "@mui/icons-material/SportsBar";
+import { useNavigate } from "react-router-dom";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import styles from "./BeerList.module.css";
-import { DataGrid } from '@mui/x-data-grid';
-
-
+import { DataGrid } from "@mui/x-data-grid";
 
 const ITEMS_PER_PAGE = 15;
 
-
 const BeerList = () => {
   const navigate = useNavigate();
+
   const [beerList, setBeerList] = useState<Array<Beer>>([]);
-  const [filterText, setFilterText] = useState<string>("");
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const [sortBy, setSortBy] = useState<string | null>(null);
+
+  // const [filterText, setFilterText] = useState<string>("");
+  // const [currentPage, setCurrentPage] = useState<number>(1);
+  // const [sortBy, setSortBy] = useState<string | null>(null);
 
   // eslint-disable-next-line
   useEffect(fetchData.bind(this, setBeerList), []);
 
-  
   // const totalPages = Math.ceil(beerList.length / ITEMS_PER_PAGE);
 
-  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFilterText(event.target.value);
-    setCurrentPage(1);
-  };
+  // const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setFilterText(event.target.value);
+  //   setCurrentPage(1);
+  // };
 
-  const handleSort = (criteria: string) => {
-    setSortBy(criteria);
+  // const handleSort = (criteria: string) => {
+  //   setSortBy(criteria);
 
-    setCurrentPage(1);
-  };
+  //   setCurrentPage(1);
+  // };
 
-  const sortBeerList = (list: Beer[]) => {
-    if (sortBy === 'name') {
-      return list.slice().sort((a, b) => a.name.localeCompare(b.name));
+  // const sortBeerList = (list: Beer[]) => {
+  //   if (sortBy === 'name') {
+  //     return list.slice().sort((a, b) => a.name.localeCompare(b.name));
+  //   }
+  //   return list;
+  // };
+
+  // const filteredBeerList = beerList.filter((beer) =>
+  //   beer.name.toLowerCase().includes(filterText.toLowerCase())
+  // );
+
+  // const sortedBeerList = sortBeerList(filteredBeerList);
+  // const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  // const endIndex = startIndex + ITEMS_PER_PAGE;
+  // const currentBeerList = sortedBeerList.slice(startIndex, endIndex);
+
+  // const onBeerClick = (id: string) => navigate(`/beer/${id}`);
+
+  const onRowClick = (params: { id: any }) => {
+    if (params.id) {
+      navigate(`/beer/${params.id}`);
     }
-    return list;
   };
-
-
-  
-  const filteredBeerList = beerList.filter((beer) =>
-    beer.name.toLowerCase().includes(filterText.toLowerCase())
-  );
-
-  const sortedBeerList = sortBeerList(filteredBeerList);
-
-
-  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-  const endIndex = startIndex + ITEMS_PER_PAGE;
-  const currentBeerList = sortedBeerList.slice(startIndex, endIndex);
-
-  const onBeerClick = (id: string) => navigate(`/beer/${id}`);
-
-
-
-
-
-
-
-
-
-
   const columns = [
-    // { field: 'id', headerName: 'ID', width: 190 },
     {
-      field: 'Order',
-      Name: 'Order',
+      field: "Order",
+      Name: "Order",
       width: 70,
       editable: true,
     },
     {
-      field: 'Name',
-      Name: 'Name',
+      field: "Name",
+      Name: "Name",
       width: 150,
       editable: true,
     },
     {
-      field: 'Type',
-      headerName: 'Type',
+      field: "Type",
+      headerName: "Type",
       width: 100,
       editable: true,
     },
     {
-      field: 'City',
-      headerName: 'City',
-      type: 'number',
+      field: "City",
+      headerName: "City",
+      type: "number",
       width: 150,
       editable: true,
     },
     {
-      field: 'Country',
-      headerName: 'Country',
-      type: 'number',
+      field: "Country",
+      headerName: "Country",
+      type: "number",
       width: 150,
       editable: true,
     },
     {
-      field: 'Phone',
-      headerName: 'Phone',
-      type: 'number',
+      field: "Phone",
+      headerName: "Phone",
+      type: "number",
       width: 150,
       editable: true,
     },
-   
-   
   ];
-
-
 
   const rows = beerList.map((beer, index) => ({
     Order: index + 1,
@@ -123,11 +116,8 @@ const BeerList = () => {
     Type: beer.brewery_type,
     City: beer.city,
     Country: beer.country,
-    Phone: beer.phone
-
+    Phone: beer.phone,
   }));
-
-
 
   return (
     <article>
@@ -136,77 +126,74 @@ const BeerList = () => {
           <h1>BeerList page</h1>
         </header>
         <main>
+          <div style={{ height: 400, width: "100%" }}>
+            <DataGrid
+              sx={{
+                // disable cell selection style
+                ".MuiDataGrid-cell:focus": {
+                  outline: "none",
+                },
+                // pointer cursor on ALL rows
+                "& .MuiDataGrid-row:hover": {
+                  cursor: "pointer",
+                  backgroundColor: "lightblue",
+                },
+              }}
+              rows={rows}
+              columns={columns}
+              checkboxSelection={false}
+              onCellClick={(params) => onRowClick(params)}
+            />
+          </div>
 
-
-        <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        checkboxSelection={false} 
-        // pageSize={5}
-        // rowsPerPageOptions={[5]}
-      />
-    </div>
-
-
-
-        <div>
-                <TextField
-                  label="Filter..."
-                  variant="outlined"
-                  value={filterText}
-                  onChange={handleFilterChange}
-                />
-
-{/* <DataGrid rows={rows} columns={columns} /> */}
-
-<Button variant="contained" onClick={() => handleSort('name')}>
-                  Sort by Name
-                </Button>
-              
-              </div>
+          {/* 
+          <div>
+            <TextField
+              label="Filter..."
+              variant="outlined"
+              value={filterText}
+              onChange={handleFilterChange}
+            />
+            <Button variant="contained" onClick={() => handleSort("name")}>
+              Sort by Name
+            </Button>
+          </div>
           <List>
-
-
-
             {currentBeerList.map((beer) => (
-              <ListItemButton key={beer.id} onClick={onBeerClick.bind(this, beer.id)}>
+              <ListItemButton
+                key={beer.id}
+                onClick={onBeerClick.bind(this, beer.id)}
+              >
                 <ListItemAvatar>
                   <Avatar>
                     <SportsBar />
                   </Avatar>
                 </ListItemAvatar>
-                {/* <ListItemText primary={beer.name} secondary={beer.brewery_type} /> */}
-                <ListItemText primary={`${beer.name} ${beer.city}`} secondary={beer.brewery_type}  />
-
+                <ListItemText
+                  primary={`${beer.name} ${beer.city}`}
+                  secondary={beer.brewery_type}
+                />
               </ListItemButton>
             ))}
           </List>
           <div className={styles.pagination}>
-                <Button
-                  disabled={currentPage === 1}
-                  onClick={() => setCurrentPage(currentPage - 1)}
-                  startIcon={<ArrowBackIcon />}
-                  variant="contained"
-                >
-                  Previous Page
-                </Button>
-                <Button
-                  disabled={endIndex >= filteredBeerList.length}
-                  onClick={() => setCurrentPage(currentPage + 1)}
-                  endIcon={<ArrowForwardIcon />}
-                  variant="contained"
-                >
-                  Next Page
-                </Button>
-
-                <span>
-                  {" "}
-                  {/* ( Page {currentPage} of {totalPages} ){" "} */}
-                </span>
-              </div>
-
-          
+            <Button
+              disabled={currentPage === 1}
+              onClick={() => setCurrentPage(currentPage - 1)}
+              startIcon={<ArrowBackIcon />}
+              variant="contained"
+            >
+              Previous Page
+            </Button>
+            <Button
+              disabled={endIndex >= filteredBeerList.length}
+              onClick={() => setCurrentPage(currentPage + 1)}
+              endIcon={<ArrowForwardIcon />}
+              variant="contained"
+            >
+              Next Page
+            </Button>
+          </div> */}
         </main>
       </section>
     </article>
